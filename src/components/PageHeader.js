03bb82e7a5bbae04027f43bbf286613b08dad01a
rahuls79 <rahul.rahulr.sinha@gmail.com>
2022-11-20 00:44:54 +0530

    git config ignorecase false

---
+++ src/components/PageHeader.js
@@ -0,0 +1,27 @@
+import "./PageHeader.css";
+
+<link rel="stylesheet"
+          href="https://fonts.google.com/specimen/Inter"></link>
+
+const PageHeader = () =>{
+
+    return(
+        <>
+        <div className="newsHead">
+            <div className="logo">
+                <p>W.</p>
+            </div>
+            <div className="newsHead-nav">
+                <button>Home</button>
+                <button>New</button>
+                <button>Popular</button>
+                <button>Trending</button>
+                <button>Categories</button>
+            </div>
+        
+        </div>
+        </>
+    )
+}
+
+export default PageHeader;
